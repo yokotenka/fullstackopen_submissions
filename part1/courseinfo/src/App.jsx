c1efc2059d3bca330167728b10aa036990c364a3
Kenta Yokote <70961284+yokotenka@users.noreply.github.com>
2024-10-02 11:55:04 +1000

    followed course until part1c

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -13,36 +13,112 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props.parts[0])
   return(
   <div>
-    <Part name={props.name1} num={props.num1} />
-    <Part name={props.name2} num={props.num2} />
-    <Part name={props.name3} num={props.num3} />
+    <Part name={props.parts[0].name} num={props.parts[0].exercises} />
+    <Part name={props.parts[1].name} num={props.parts[1].exercises} />
+    <Part name={props.parts[2].name} num={props.parts[2].exercises} />
   </div>)
 }
 
 const Total = (props) => {
-  return(<p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>)
+  return(<p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>)
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+}
 
   return (
     <div>
-      <Header course={course} />
-      <Content name1={part1} name2={part2}  name3={part3} num1={exercises1}
-        num2={exercises2}
-        num3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
+
+
+// // variables
+// const x = 1
+// let y = 5
+
+// console.log(x, y)   // 1, 5 est affiché
+// y += 10
+// console.log(x, y)   // 1, 15 est affiché
+// y = 'sometext'
+// console.log(x, y)   // 1, sometext est affiché
+// // x = 4               // provoque une erreur
+
+
+// // arrays
+// console.log("Arrays")
+
+// const t = [1, -1, 3]
+// console.log("Const array before push", t)
+// t.push(5)
+
+// console.log("Length of array after push", t.length) // 4 est affiché
+// console.log("First element of array", t[1])     // -1 est affiché
+// console.log("Iterate through each element")
+// t.forEach(value => {
+//   console.log(value)  // les chiffres 1, -1, 3, 5 sont affichés, chacun sur une ligne
+// })    
+
+
+// console.log("Concat behaviour")
+// const t1 = [1, -1, 3]
+// const t2 = t.concat(5)
+
+// console.log("The original array", t1)  // [1, -1, 3] est affiché
+// console.log("After concat", t2) // [1, -1, 3, 5] est affiché
+
+
+// console.log("Map behaviour")
+// console.log(t)
+// const m1 = t.map(value => value * 2)
+// console.log(m1)   // [2, 4, 6] est affiché
+
+// console.log("Changing array to array of HTML Strings")
+// const m2 = t.map(value => '<li>' + value + '</li>')
+// console.log(m2)  
+// // [ '<li>1</li>', '<li>2</li>', '<li>3</li>' ] est affiché
+
+// console.log("destructuring array")
+// const t4 = [1,2,3,4,5]
+// const [first, second, ...rest] = t4
+// console.log(first, second)  // 1, 2 est affiché
+// console.log(rest)          // [3, 4, 5] est affiché
+
+// console.log("Other methods to define functions")
+// function product(a, b) {
+//   return a * b
+// }
+
+// const result1 = product(2, 6)
+// // result est maintenant 12
+
+// const average = function(a, b) {
+//   return (a + b) / 2
+// }
+
+// const result2 = average(2, 5)
+// // result est maintenant 3.5
 
 export default App
--- part1/tests/src/App.jsx
+++ part1/tests/src/App.jsx
@@ -4,37 +4,125 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
+// const Hello = (props) => {
+//   return (
+//     <div>
+//       <p>
 
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
+//         Hello {props.name}, you are {props.age} years old
+//       </p>
+//     </div>
+//   )
+// }
 
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
-    </div>
-  )
-}
+// const Footer = () => {
+//   return (
+//     <div>
+//       greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+
+//   const name = 'Peter'
+//   const age = 10
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name="John" age={9} />
+//       <Hello name="Maya" age={26 + 10} />
+//       <Footer />
+//     </div>
+//   )
+// }
+
+//section C
+// const Hello = ({ name, age }) => {
+//   const bornYear = () => new Date().getFullYear() - age
+
+//   return (
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
+//       <p>So you were probably born in {bornYear()}</p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const name = 'Peter'
+//   const age = 10
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name="Maya" age={26 + 10} />
+//       <Hello name={name} age={age} />
+//     </div>
+//   )
+// }
+
+
+// const App = (props) => {
+//   const {counter} = props
+//   return (
+//     <div>{counter}</div>
+//   )
+// }
+
+
+
+// export default App
+
+// stateful compenent 
+// const App = () => {
+  
+//   // userState. The initial value is ignored in all subsequent calls. Only used during the initial call.
+//   // setCounter function will rerender the component that it is housed in.
+//   const [ counter, setCounter ] = useState(0)
+//   console.log('called', counter)
+//   setTimeout(
+//     () => setCounter(counter + 1),
+//     1000
+//   )
+//   console.log('rendering...', counter)
+//   return (
+//     <div>{counter}</div>
+//   )
+// }
+
+const Display = ({ counter }) => <div>{counter}</div>
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
+  const [ counter, setCounter ] = useState(0)
 
-  const name = 'Peter'
-  const age = 10
+  const increaseByOne = () => setCounter(counter + 1)
+
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="John" age={9} />
-      <Hello name="Maya" age={26 + 10} />
-      <Footer />
+      <Display counter={counter}/>
+
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
   )
 }
 export default App
+
